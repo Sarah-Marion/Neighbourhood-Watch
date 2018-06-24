e2e203f3541b44593f1116d021b53d83e79fc3d5
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-24 13:26:21 +0300

    created a routing for signing up

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from .forms import *
+import json
+import requests
+from .models import *
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.forms.models import inlineformset_factory
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from googlemaps import Client, convert
+from django.conf import settings
+from .decorators import user_belongs_to_hood
+
 
 # Create your views here.
+def signup(request):
+    current_user = request.user
+    if current_user.is_authenticated():
+        return HttpResponseRedirect('/')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your Neighbourhood Watch Account'
+            message = render_to_string('registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect('account_activation_sent')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
