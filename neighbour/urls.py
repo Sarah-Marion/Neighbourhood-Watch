04eec755bd236b36b758c3c5d3b94c4b3aa5085e
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-25 17:40:17 +0300

    setup routings for registration and activations

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+import hood.views as app_views
+from hood.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('hood.urls')),
+    url(r'^login/$', views.login, {'template_name': 'registration/login.html',
+                                   "authentication_form": LoginForm}, name='login'),
+    url(r'^account_activation_sent/$', app_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        app_views.activate, name='activate'),
+    url(r'^signup/$', app_views.signup, name='signup'),
+    url(r'^logout/$', views.logout, {'next_page': 'login'}, name='logout'),
+    # url(r'^tinymce/', include('tinymce.urls')),
 ]
