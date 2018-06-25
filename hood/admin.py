6fdba55c1528e2685411a17e7a1d22565bdf66b1
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-25 17:17:57 +0300

    registered te application models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, Hood, Location, Business, News
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Hood)
+admin.site.register(Location)
+admin.site.register(Business)
+admin.site.register(News)
